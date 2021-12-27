a0b0796c3f4897436f199d446adf84787100c63d
Yeganathan S <hilariousengineer@protonmail.com>
2021-12-28 02:39:06 +0530

    Add TailwindCSS for the autoFill component

--- autoFill/src/App.js
+++ autoFill/src/App.js
@@ -1,13 +1,12 @@
-import "./App.css";
+import "./index.css";
 import AutoFill from "./AutoFill";
 import top100Films from "./top100flims";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <AutoFill props={top100Films} />
-      </header>
+    <div className="container">
+      <h3 className="text-center pt-4 text-4xl"> React autoFill Component in ES6</h3>
+      <AutoFill props={top100Films} />
     </div>
   );
 }
--- autoFill/src/AutoFill.js
+++ autoFill/src/AutoFill.js
@@ -1,33 +1,61 @@
-import * as React from 'react';
-import { useState } from 'react';
-import TextField from '@mui/material/TextField';
-import Autocomplete from '@mui/material/Autocomplete';
+/* eslint-disable no-unused-vars */
+import * as React from "react";
+import { useState } from "react";
 
 
-const AutoFill = (top100Films) => {
+const AutoFill = (props) => {
+  const [input, setInput] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
 
-  const [value, setValue] = useState('');
-  const [inputValue, setInputValue] = useState('');
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
 
+  const handleClick = (e) => {
+    const { label, year } = e.target.dataset;
+    setInput(label);
+    setSuggestions([]);
+  };
+
+  const filterSuggestions = (input) => {
+    return props.props.filter((film) =>
+      film.label.toLowerCase().includes(input.toLowerCase())
+    );
+  };
   return (
-    <div>
-      <div style={{color: "black"}}>{`InputValue: '${inputValue}'`}</div>
-      <br />
-      <Autocomplete
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-        inputValue={inputValue}
-        onInputChange={(event, newInputValue) => {
-          setInputValue(newInputValue);
-        }}
-        id="controllable-states-demo"
-        options={top100Films.props}
-        sx={{ width: 300 }}
-        renderInput={(params) => <TextField {...params} label="type something.." />}
-      />
+    <div className="md:container md:mx-auto pt-4">
+      <div>
+        <input
+          type="text"
+          value={input}
+          onChange={handleChange}
+          className="border border-gray-400 p-2 w-full"
+        />
+      </div>
+      <div className="pt-4 grid justify-items-center">
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded w-40"
+          onClick={() => setInput("")}
+        >
+          Clear
+        </button>
+      </div>
+      <div className="text-center pt-4 ">
+        {filterSuggestions(input).map((film) => (
+          <ul key={film.label}>
+            <li
+              data-label={film.label}
+              data-year={film.year}
+              onClick={handleClick}
+              className="text-blue-500 cursor-pointer text-lg hover:text-violet-700"
+            >
+              {film.label} ({film.year})
+            </li>
+          </ul>
+        ))}
+      </div>
     </div>
   );
 };
+
 export default AutoFill;
